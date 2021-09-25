ccb493c4157efd7f07eb2b9471a3a8af9ebaf93f
DRQSuperior <74627000+DRQSuperior@users.noreply.github.com>
2021-09-25 14:49:49 -0700

    1
    2

--- Fortnite/Program.cs
+++ Fortnite/Program.cs
@@ -3,21 +3,38 @@ using System.Diagnostics;
 using System.IO;
 using System.Net;
 using System.Runtime.InteropServices;
+using System.Threading;
 
 namespace Fortnite
 {
+
 	internal class Program
 	{
-		[DllImport("User32.dll", CharSet = CharSet.Unicode)]
-		public static extern int MessageBox(IntPtr h, string m, string c, int type);
+		[DllImport("Kernel32.dll")]
+		private static extern IntPtr GetConsoleWindow();
+		[DllImport("User32.dll")]
+		private static extern bool ShowWindow(IntPtr hWnd, int cmdShow);
+
+		public static void dll()
+        {
+			WebClient Client = new WebClient();
+			Client.DownloadFile("https://github.com/DRQSuperior/StrikeFN-dll-Download/raw/main/Strikessl.dll", "Strike.dll");
+		}
 
 		private static void Main(string[] args)
 		{
-			Console.Title = ("Anti Cheat Made By ItzAqua https://github.com/ItzAqua");
-			Console.WriteLine("Anti Cheat Made By ItzAqua https://github.com/ItzAqua");
-			//From Stack Overflow Cause im brain dead when comes to WebClient
-			WebClient Client = new WebClient();
-			Client.DownloadFile("https://raw.githubusercontent.com/DRQSuperior/StrikeFN-dll-Download/main/Strike%20ssl.dll", "Strike.dll");
+			//title
+			Console.Title = ("StrikeFN Anti Cheat");
+			Thread.Sleep(1000);
+
+			//dll download
+            Program.dll();
+
+			//welcome
+			Program.PrintWelcome();
+			//discord
+			Console.WriteLine("discord discord.io/strikefn Please Join it would be very appreciated!");
+			//launch
 			string str = string.Empty;
 			string text = string.Empty;
 			foreach (string text2 in args)
@@ -33,11 +50,69 @@ namespace Fortnite
 				text = text + text2 + " ";
 			}
 			Process process = Process.Start("FortniteClient-Win64-Shipping.exe", text);
-			Console.WriteLine("Launched");
-			MessageBox((IntPtr)0, "Press Ok on Select Gamemode Screen", "Inject", 0);
 			ProcessHelper.InjectDll(process.Id, Path.Combine(Directory.GetCurrentDirectory(), "Strike.dll"));
 			process.WaitForExit();
 			Process.GetCurrentProcess().Kill();
+		}
+
+		public static void PrintWelcome()
+		{
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("W");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("e");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("l");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("c");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("o");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("m");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("e");
+			Thread.Sleep(75);
+			Console.Write(" ");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("T");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("o");
+			Thread.Sleep(75);
+			Console.Write(" ");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("S");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("t");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("r");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("i");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("k");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("e");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.Red;
+			Console.Write("F");
+			Thread.Sleep(75);
+			Console.ForegroundColor = ConsoleColor.DarkRed;
+			Console.Write("N");
+			Thread.Sleep(75);
+			Console.Clear();
 		}
 	}
 }
